193e519019c13fe0983514818b6332085260ddd4
penshuilong <jeffrey.zhao0112@hotmail.com>
2023-01-24 18:42:24 -0500

    111

--- game_main.c
+++ game_main.c
@@ -1,7 +1,7 @@
 /*  ========== H02 - Guessing Game ============
  *
- *   Student: SOLUTION
- *   Semester:
+ *   Student: Zefeng Zhao
+ *   Semester: 23 Spring
  *
  *   A simple guessing game program. This
  *   Uses a separate main file where system
@@ -18,6 +18,56 @@
 
 /* Program entry point*/
 int main() {
-    srand(time(NULL)); // to make the game random, purposely set here only! 
-    return start_game();
+
+    int outCount = 0;
+    int cntEachRound[5];
+    int j = 0;
+    printf("%s\n", "====================================================");
+    printf("%s\n", "| Welcome to the Guessing Game. Let's get started. |");
+    printf("%s\n", "====================================================");
+    while(j < 5){
+        cntEachRound[j] = 0;
+        j++;
+    }
+
+    // round
+    while(outCount < 5){
+        printf("%s\n", "====================================================");
+        printf("|                     Round %d:                     |\n", outCount+1);
+        printf("%s\n", "====================================================");
+        unsigned int seed = time(NULL);
+        srand(seed);
+        int random = rand()%10+1;
+        //  time
+        
+        while(1){
+            printf("%s", "Guess a number (1 to 10):");
+            int num;
+            scanf("%d", &num);
+            cntEachRound[outCount]++;
+            if(num == random){
+            printf("%s\n", "Congratulations, you found it!");
+            break;
+            }else if(num < random){
+            printf("%s\n", "Your guess is too low.");
+            }else{
+            printf("%s\n", "Your guess is too high.");
+            }
+        }
+
+        outCount++;
+    }
+
+    int i = 0;
+    printf("%s\n", "====================================================");
+    printf("%s\n", "|             Result of the round                  |");
+    printf("%s\n", "====================================================");
+
+    while(i < 5){
+        printf("Round %d took you %d guesses.\n", i+1, cntEachRound[i]);
+
+        i++;
+    }
+
+    return 0;
 }
